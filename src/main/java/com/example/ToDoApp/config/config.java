bc38277dce56c0bea0da096a36589568a6509198
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-02 11:21:19 +0700

    create 2 bean rolePageable and userPageable in config.java

--- src/main/java/com/example/ToDoApp/config/config.java
+++ src/main/java/com/example/ToDoApp/config/config.java
@@ -1,7 +1,19 @@
 package com.example.ToDoApp.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 
 @Configuration
 public class config {
+    @Bean
+    public PageRequest rolePageable() {
+        return PageRequest.of(0, 20, Sort.by(Sort.Direction.ASC, "roleName"));
+    }
+
+    @Bean
+    public PageRequest userPageable() {
+        return PageRequest.of(0, 20, Sort.by(Sort.Direction.ASC, "username"));
+    }
 }
