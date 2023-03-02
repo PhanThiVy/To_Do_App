1af5ad5920ec7196d702614f0ea03d18fb4480c2
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-02 10:04:04 +0700

    add context for User class

--- src/main/java/com/example/ToDoApp/model/User.java
+++ src/main/java/com/example/ToDoApp/model/User.java
@@ -1,4 +1,32 @@
 package com.example.ToDoApp.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Data
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+    private String password;
+    @ManyToMany(cascade =
+            {
+                    CascadeType.DETACH,
+                    CascadeType.MERGE,
+                    CascadeType.REFRESH,
+                    CascadeType.PERSIST
+            }, fetch = FetchType.LAZY)
+    @JoinTable(
+            name = "user_role",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private List<Role> role = new ArrayList<>();
 }
