5a41399447a846aa97c16d05e48fe248a9567803
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-02 10:02:32 +0700

    change name Role for class role

--- src/main/java/com/example/ToDoApp/model/role.java
+++ src/main/java/com/example/ToDoApp/model/role.java
@@ -3,12 +3,9 @@ package com.example.ToDoApp.model;
 import jakarta.persistence.*;
 import lombok.Data;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Data
 @Entity
-public class role {
+public class Role {
     @Id
     @GeneratedValue
     private Long id;
