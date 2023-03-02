376c848a6ea53f7cdce52c91b148f71b67e69d17
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-02 10:33:25 +0700

    update content for  RoleRequestDto.java

--- src/main/java/com/example/ToDoApp/dto/requestDto/RoleRequestDto.java
+++ src/main/java/com/example/ToDoApp/dto/requestDto/RoleRequestDto.java
@@ -1,4 +1,14 @@
 package com.example.ToDoApp.dto.requestDto;
 
+import jakarta.validation.constraints.NotEmpty;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
 public class RoleRequestDto {
+    @NotEmpty
+    private String roleName;
 }
