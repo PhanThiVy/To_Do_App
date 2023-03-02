50b3d1ff174536d42e9a97098e4050bf5ef030f7
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-02 15:18:52 +0700

    create method findAll in RoleRepository

--- src/main/java/com/example/ToDoApp/repository/RoleRepository.java
+++ src/main/java/com/example/ToDoApp/repository/RoleRepository.java
@@ -1,10 +1,16 @@
 package com.example.ToDoApp.repository;
 
 import com.example.ToDoApp.model.Role;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
 @Repository
 public interface RoleRepository extends JpaRepository<Role,Long> {
+    public Page<Role> findAll(Pageable pageable);
 }
