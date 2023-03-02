298c97ba04991ef0ff6bc193aa5cfd7b2296e581
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-02 11:57:58 +0700

    create API '/add' for RoleController

--- src/main/java/com/example/ToDoApp/controller/RoleController.java
+++ src/main/java/com/example/ToDoApp/controller/RoleController.java
@@ -1,15 +1,26 @@
 package com.example.ToDoApp.controller;
 
+import com.example.ToDoApp.dto.requestDto.RoleRequestDto;
+import com.example.ToDoApp.dto.responseDto.RoleResponseDto;
 import com.example.ToDoApp.service.RoleServiceImpl;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("role")
+@RequestMapping("/role")
 @RequiredArgsConstructor
 public class RoleController {
     private final RoleServiceImpl roleService;
+    @PostMapping("/add")
+    public ResponseEntity<RoleResponseDto> addRole(@RequestBody RoleRequestDto roleRequestDto){
+        RoleResponseDto roleResponseDto = roleService.addRole(roleRequestDto);
+        return new ResponseEntity<>(roleResponseDto, HttpStatus.OK);
+    }
 
 
 }
