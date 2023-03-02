e69badfe9feb1af9cc5c321d3b15f7eb2de41c3e
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-02 11:07:45 +0700

    create method roleToRoleResponseDto(Role role) in Mapper.java

--- src/main/java/com/example/ToDoApp/dto/mapper/Mapper.java
+++ src/main/java/com/example/ToDoApp/dto/mapper/Mapper.java
@@ -1,4 +1,27 @@
 package com.example.ToDoApp.dto.mapper;
 
+import com.example.ToDoApp.dto.responseDto.RoleResponseDto;
+import com.example.ToDoApp.model.Role;
+import com.example.ToDoApp.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Mapper {
+    public static RoleResponseDto roleToRoleResponseDto(Role role){
+        RoleResponseDto roleResponseDto =new RoleResponseDto();
+
+        roleResponseDto.setId(role.getId());
+        roleResponseDto.setRoleName(role.getRoleName());
+
+        List<String> userNames = new ArrayList<>();
+        List<User> users = role.getUserList();
+        for (User user: users) {
+            userNames.add(user.getUsername());
+        }
+        roleResponseDto.setUserNames(userNames);
+
+        return roleResponseDto;
+
+    }
 }
