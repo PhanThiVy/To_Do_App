51bc1062b52b11345e1bf08ffce726b4fd991380
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-02 14:56:56 +0700

    add @Repository to RoleRepository and UserRepository

--- src/main/java/com/example/ToDoApp/repository/RoleRepository.java
+++ src/main/java/com/example/ToDoApp/repository/RoleRepository.java
@@ -5,6 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
-
+@Repository
 public interface RoleRepository extends JpaRepository<Role,Long> {
 }
