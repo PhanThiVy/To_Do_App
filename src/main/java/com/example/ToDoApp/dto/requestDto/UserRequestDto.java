7a69e9fe03cde3043e2cac3bf8560a04d6aec783
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-02 10:51:48 +0700

    add content for UserRequestDto

--- src/main/java/com/example/ToDoApp/dto/requestDto/UserRequestDto.java
+++ src/main/java/com/example/ToDoApp/dto/requestDto/UserRequestDto.java
@@ -1,4 +1,25 @@
 package com.example.ToDoApp.dto.requestDto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+import lombok.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+@Builder
 public class UserRequestDto {
+    @NotEmpty
+    @Size(min = 5,max = 60)
+    private String username;
+
+    @NotEmpty
+    @Size(min = 8,max = 20)
+    private String password;
+
+    @NotEmpty
+    @Email
+    private String email;
 }
