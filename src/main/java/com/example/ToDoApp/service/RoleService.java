2d7e2d655f14297e75002eeaa004948969711e5c
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-02 11:27:09 +0700

    add method for UserService

--- src/main/java/com/example/ToDoApp/service/RoleService.java
+++ src/main/java/com/example/ToDoApp/service/RoleService.java
@@ -1,7 +1,19 @@
 package com.example.ToDoApp.service;
 
+import com.example.ToDoApp.dto.requestDto.RoleRequestDto;
+import com.example.ToDoApp.dto.responseDto.RoleResponseDto;
+import com.example.ToDoApp.model.Role;
+import com.example.ToDoApp.model.User;
 import org.springframework.data.domain.Page;
 
 public interface RoleService {
-
+    public RoleResponseDto addRole(RoleRequestDto roleRequestDto);
+    public Page<RoleResponseDto> getRoleList(int pageNumber);
+    public Role getRole(Long roleId);
+    public RoleResponseDto getRoleById(Long roleId);
+    public void deleteRole(Long roleId);
+    public RoleResponseDto editRole(Long roleId, RoleRequestDto roleRequestDto);
+    public Page<RoleResponseDto> searchByRoleName(String roleName,int pageNumber);
+    void addBook(Role role, User user);
+    void removeBook(Role role, User user);
 }
