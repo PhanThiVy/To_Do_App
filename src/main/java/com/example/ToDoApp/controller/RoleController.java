496262a200e6518c24c514d1d2758d1e2b4371db
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-02 11:40:14 +0700

    create reolService  property

--- src/main/java/com/example/ToDoApp/controller/RoleController.java
+++ src/main/java/com/example/ToDoApp/controller/RoleController.java
@@ -1,9 +1,15 @@
 package com.example.ToDoApp.controller;
 
+import com.example.ToDoApp.service.RoleServiceImpl;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("role")
+@RequiredArgsConstructor
 public class RoleController {
+    private final RoleServiceImpl roleService;
+
+
 }
