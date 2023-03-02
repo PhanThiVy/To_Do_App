2e4731b9e2f1860e209b5dedf96546483bc52c4d
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-02 11:01:01 +0700

    add content for UserResponseDto.java

--- src/main/java/com/example/ToDoApp/dto/responseDto/UserResponseDto.java
+++ src/main/java/com/example/ToDoApp/dto/responseDto/UserResponseDto.java
@@ -1,4 +1,20 @@
 package com.example.ToDoApp.dto.responseDto;
 
-public class UserResponseDto q{
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Setter
+@Getter
+public class UserResponseDto {
+    private String id;
+    private String username;
+    private String password;
+    private String email;
+    private List<String> roleNames;
 }
