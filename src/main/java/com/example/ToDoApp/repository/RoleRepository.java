e32b4fd30577f04aecec260899c7fc678d361453
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-03 15:22:24 +0700

    create method roleNameIsExistForEdit

--- src/main/java/com/example/ToDoApp/repository/RoleRepository.java
+++ src/main/java/com/example/ToDoApp/repository/RoleRepository.java
@@ -4,6 +4,8 @@ import com.example.ToDoApp.model.Role;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,4 +15,7 @@ import java.util.List;
 @Repository
 public interface RoleRepository extends JpaRepository<Role,Long> {
     public Page<Role> findAll(Pageable pageable);
+    public Role findRoleByRoleNameIgnoreCase(String roleName);
+    @Query(value = "SELECT * FROM role r WHERE r.role_name = :roleName and r.id != :roleId", nativeQuery = true)
+    Role roleNameIsExistForEdit(@Param("roleName") String roleName, @Param("roleId") Long roleId);
 }
