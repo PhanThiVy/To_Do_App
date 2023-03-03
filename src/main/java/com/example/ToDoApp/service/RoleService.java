db688cbdd6aad21237d24dd38c88ade25246a24d
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-03 15:17:27 +0700

    add method roleNameIsExist and roleNameIsExistForEdit

--- src/main/java/com/example/ToDoApp/service/RoleService.java
+++ src/main/java/com/example/ToDoApp/service/RoleService.java
@@ -9,12 +9,14 @@ import org.springframework.data.domain.Page;
 public interface RoleService {
     public RoleResponseDto addRole(RoleRequestDto roleRequestDto);
     public Page<RoleResponseDto> getRoleList(int pageNumber);
-    public boolean isNumberic(String roleId);
     public Role getRole(String roleId);
     public RoleResponseDto getRoleById(String roleId);
     public void deleteRole(String roleId);
     public RoleResponseDto editRole(String roleId, RoleRequestDto roleRequestDto);
     public Page<RoleResponseDto> searchByRoleName(String roleName,int pageNumber);
-    void addBook(Role role, User user);
-    void removeBook(Role role, User user);
+    void addUser(Role role, User user);
+    void removeUser(Role role, User user);
+    public boolean isNumberic(String roleId);
+    public void roleNameIsExist(String roleName);
+    public void roleNameIsExistForEdit(String roleName,Long roleId);
 }
