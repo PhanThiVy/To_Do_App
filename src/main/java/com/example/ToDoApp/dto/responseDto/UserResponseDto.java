5417d38195dd1eb4f7f0c801bf147934b986f948
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-04 12:44:42 +0700

    update UserResponse

--- src/main/java/com/example/ToDoApp/dto/responseDto/UserResponseDto.java
+++ src/main/java/com/example/ToDoApp/dto/responseDto/UserResponseDto.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @AllArgsConstructor
@@ -13,8 +14,11 @@ import java.util.List;
 @Getter
 public class UserResponseDto {
     private Long id;
-    private String username;
-    private String password;
+    private String fullName;
+    private String userName;
     private String email;
     private List<String> roleNames;
+    private LocalDate registrationDate;
+    private Boolean locked;
+    private Boolean enabled;
 }
