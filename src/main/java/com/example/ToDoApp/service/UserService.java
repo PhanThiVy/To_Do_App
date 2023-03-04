480df60ed5e17d7cde36c3d18995a2bcf82542a5
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-04 12:38:59 +0700

    upadte UserService

--- src/main/java/com/example/ToDoApp/service/UserService.java
+++ src/main/java/com/example/ToDoApp/service/UserService.java
@@ -1,2 +1,15 @@
-package com.example.ToDoApp.service;public class UserService {
+package com.example.ToDoApp.service;
+
+import com.example.ToDoApp.dto.requestDto.UserRequestDto;
+import com.example.ToDoApp.dto.responseDto.UserResponseDto;
+import org.springframework.stereotype.Service;
+
+@Service
+public interface UserService {
+    public UserResponseDto addUser(UserRequestDto userRequestDto);
+    public UserResponseDto addRoleToUserToUser(String userId, String roleId);
+    public UserResponseDto deleteRoleFromUser(String bookId, String roleId);
+    public boolean isNumberic(String userId);
+    public boolean userNameIsExist(String userName);
+
 }
