2b0493983293e7dcdb339d7acf34f52c42432adf
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-04 12:34:38 +0700

    create UserNameExistException

--- src/main/java/com/example/ToDoApp/exception/UserNameExistException.java
+++ src/main/java/com/example/ToDoApp/exception/UserNameExistException.java
@@ -1,2 +1,18 @@
-package com.example.ToDoApp.exception;public class UserNameExistException {
+package com.example.ToDoApp.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class UserNameExistException extends RuntimeException{
+    private final ErrorDetail errorDetail;
+
+    public UserNameExistException(int errorCode, String message) {
+        super();
+        this.errorDetail = new ErrorDetail().builder()
+                .errorCode(errorCode)
+                .message(message)
+                .build();
+    }
 }
