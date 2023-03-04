9b8fcedfd07500102412faa1d979d1deef2ab427
PhanThiVy <phanthithaovy2001@gmail.com>
2023-03-04 12:42:37 +0700

    creat API /user/add

--- src/main/java/com/example/ToDoApp/controller/UserController.java
+++ src/main/java/com/example/ToDoApp/controller/UserController.java
@@ -1,9 +1,24 @@
 package com.example.ToDoApp.controller;
 
+import com.example.ToDoApp.dto.requestDto.UserRequestDto;
+import com.example.ToDoApp.dto.responseDto.UserResponseDto;
+import com.example.ToDoApp.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/user")
+@RequiredArgsConstructor
 public class UserController {
+    private final UserService userService;
+    @PostMapping("add")
+    public ResponseEntity<UserResponseDto> addUser(@RequestBody UserRequestDto userRequestDto){
+        UserResponseDto userResponseDto = userService.addUser(userRequestDto);
+        return new ResponseEntity<>(userResponseDto, HttpStatus.OK);
+    }
 }
